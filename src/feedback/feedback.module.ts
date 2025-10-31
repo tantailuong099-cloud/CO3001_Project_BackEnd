468f754a052411cb5179814832a3e5e46ef57e05
Nguyen Minh Quan <minhquanvoh@gmail.com>
2025-10-31 22:00:22 +0700

    Implement Feeback schema - controller - module and create DTO for feedback

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -3,12 +3,16 @@ import { FeedbackController } from './feedback.controller';
 import { FeedbackService } from './feedback.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FeedBack, FeedBackSchema } from './schema/feedback.schema';
+import { UserModule } from '@/user/user.module';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: FeedBack.name, schema: FeedBackSchema },
     ]),
+    UserModule,
+    AuthModule,
   ],
   controllers: [FeedbackController],
   providers: [FeedbackService],
--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { FeedBack, FeedBackDocument } from './schema/feedback.schema';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
+import { UserService } from '@/user/user.service';
 
 @Injectable()
 export class FeedbackService {
   constructor(
     @InjectModel(FeedBack.name) private feedbackModel: Model<FeedBackDocument>,
+    private readonly userService: UserService,
   ) {}
+
+  async create(
+    createFeedbackDto: CreateFeedbackDto,
+    authorId: string,
+  ): Promise<FeedBackDocument> {
+    const { tutor: tutorId } = createFeedbackDto;
+    await this.userService.updateUserInfo(tutorId, {});
+    const newFeedback = new this.feedbackModel({
+      // các fields trong model định nghĩa trong schema của nó
+      ...createFeedbackDto,
+      author: authorId,
+    });
+
+    return newFeedback.save();
+  }
+
+  async getForTutor(tutorId: string): Promise<FeedBackDocument[]> {
+    return this.feedbackModel
+      .find({ tutor: tutorId })
+      .populate('author', 'name email')
+      .exec();
+  }
 }
